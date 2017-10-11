9d368be7bf5e8460d92245fea2d23ba714f30ef9
Сергей Девятов <somova25@mail.ru>
2017-10-12 00:11:11 +0300

    Finish all sections. todo: animations

--- spaceport/js/base.js
+++ spaceport/js/base.js
@@ -1,7 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var hyphenate = createHyphenator(hyphenationPatternsRu);
-    
-    $('.text').each(function(i, item){
-        $(item).text(hyphenate($(item).text()));
+
+    $('.text').each(function (i, item) {
+        $(item).html(hyphenate($(item).html()));
     });
+
+    function isInView(elem) {
+        var viewTop = window.scrollY;
+        var viewBottom = window.scrollY + window.innerHeight;
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+        return viewTop < elemBottom && viewTop > elemTop || viewBottom > elemTop && viewBottom < elemBottom;
+    }
+
+    $(window).scroll(function (e) {
+        $('.video_background').each(function (i, elem) {
+            if (isInView(elem)) {
+                $(this).children('video').each((_i, _elem) => _elem.play());
+            }
+            else{
+                $(this).children('video').each((_i, _elem) => _elem.pause());
+            }
+        });
+    });
+
+    $(window).trigger('scroll');
+
+    $(window).focus(() => $('.video_background video').each((i, elem) => isInView(elem) ? elem.play() : null));
+    $(window).blur(() => $('.video_background video').each((i, elem) => elem.pause()));
 });
